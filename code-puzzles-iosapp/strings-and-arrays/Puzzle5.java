45d9bbed07286a0cf41da97f830c1a59e3841f6b
ayorhan <ayorhan@ayorhan.com>
2012-10-09 17:50:04 +0300

    puzzle 5

---
+++ code-puzzles-iosapp/strings-and-arrays/Puzzle5.java
@@ -0,0 +1,76 @@
+/**
+ * @author Arda Yigithan Orhan <ayorhan@ayorhan.com>
+ * @version 1.0
+ * @since 2012-10-09
+ **/
+
+import java.util.HashMap;
+public class Puzzle5{
+    
+    public static void main(String[] args){
+		
+	long startTime, endTime, duration = 0, total = 0;
+	String[] testCases = {null, "","aabbbbcccddddd", "abcd", "ed  de de", "dddd"};
+			      
+	int [] durations = new int [testCases.length];
+	    
+	for(String test : testCases){
+
+	    startTime = System.nanoTime();
+	    String compressed = compress(test);
+	    endTime = System.nanoTime();
+
+	    duration = endTime - startTime;
+	    System.out.println(test + " compressed to " + compressed + "  took:" +  duration);
+	    total += duration;
+	}
+       
+    }
+
+    
+    /**
+     * Compresses strings following this rule:
+     * <p> replace every group of char repeating more than one time with {char}{times}
+     *
+     * Time complexity ~ 
+     * Space complexity ~ 
+     *
+     * @param str given string 
+     * 
+     * @return compressed form of the string
+     **/
+    public static String compress(String str){
+	
+	if(str == null || str.length() == 0)
+	    return str;
+
+	int strLen = str.length();
+	int repeated = 0;
+	StringBuilder output = new StringBuilder();
+
+	for(int i=0; i<strLen; ){ // O(n) where n is the length of the string, linear time
+	    char current = str.charAt(i);
+	    output.append(current);
+
+	    repeated = 1;
+	  
+	    while(i++ < strLen-1 && current == str.charAt(i))
+		repeated++;
+		
+	    
+
+	    if(repeated > 1)	
+		output.append(repeated);
+
+	    
+	}
+
+	return output.toString();
+
+	
+    }
+    
+    
+
+
+} 
