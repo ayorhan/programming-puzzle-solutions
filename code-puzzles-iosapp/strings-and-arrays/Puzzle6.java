6d916917f22368bcc2f9c3f91eebea04cbf5bcfc
ayorhan <ayorhan@ayorhan.com>
2012-10-10 10:50:21 +0300

    puzzle 6 done with 2 solutions, one efficient in terms of space and time, one bad example

--- code-puzzles-iosapp/strings-and-arrays/Puzzle6.java
+++ code-puzzles-iosapp/strings-and-arrays/Puzzle6.java
@@ -9,19 +9,18 @@ public class Puzzle6{
     public static void main(String[] args){
 		
 	long startTime, endTime, duration = 0, total = 0;
-	int [][] decks ={ {1, 2, 3, 4}, {3, 53, 3, 5}};
-			      
+	int [][] decks ={ {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}, {3, 53, 3, 5}};
 	int [] durations = new int [decks.length];
 	    
 	for(int[] deck : decks){
 	    String pre = printDeck(deck);
 
 	    startTime = System.nanoTime();
-	    shuffle(deck);
+	    shuffle2(deck);
 	    endTime = System.nanoTime();
 
 	    duration = endTime - startTime;
-	    System.out.println(pre + " shuffled to " + printDeck(deck) + "  took:" +  duration);
+	    System.out.println("Deck: " + pre + "  took:" +  duration);
 	    total += duration;
 	}
        
@@ -31,8 +30,8 @@ public class Puzzle6{
     /**
      * Randomly shuffles a deck of cards
      *
-     * Time complexity ~ 
-     * Space complexity ~ 
+     * Time complexity ~ Quadratic O(n^2) 
+     * Space complexity ~ Linear  
      *
      * @param card array of ints representing a deck of cards 
      * 
@@ -40,20 +39,52 @@ public class Puzzle6{
     public static void shuffle(int [] cards){
 
 	int deckLen = cards.length;
-	int [] shuffled = new int[deckLen];
+	int [] shuffled = new int[deckLen]; // linear space
 	int index = 0;
-	for(int card : cards){
+	for(int card : cards){ // linear time O(n), where n is the length of the deck
 	    int newPosition = (int)(Math.random() * deckLen);
-	    while(shuffled[newPosition] != 0)
-		newPosition = (int)(Math.random() * deckLen);
+	    // worst case linear time O(m), 
+	    // where m is the length of the new deck
+	    while(shuffled[newPosition] != 0){ //collision handling
+		newPosition++;
+		if(newPosition == deckLen)
+		    newPosition = 0;
+	    }
 
 	    shuffled[newPosition] = cards[index];
 	    index++;
 	    
 	}
 
-	cards = shuffled;
+	System.out.print("Shuffled: " + printDeck(shuffled) + " ");
     }
+
+    /**
+     * Randomly shuffles a deck of cards by swapping
+     *
+     * Time complexity ~ Linear
+     * Space complexity ~ Constant  
+     *
+     * @param card array of ints representing a deck of cards 
+     * 
+     **/
+    public static void shuffle2(int [] cards){
+
+	int deckLen = cards.length;
+	int index = 0;
+	for(int card : cards){ // linear time O(n), where n is the length of the deck
+	    int newPosition = (int)(Math.random() * deckLen);
+	    
+	    // Swapping constant time
+	    int temp = cards[index];
+	    cards[index] = cards[newPosition];
+	    cards[newPosition] = temp;
+	    index++;
+	}
+
+	System.out.print("Shuffled: " + printDeck(cards) + " ");
+    }
+
 
     public static String printDeck(int [] deck){
 	int len = deck.length;
